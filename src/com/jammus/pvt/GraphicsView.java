446424ed4f6fcc6bd8a7a62af146cda86addb4a0
jammus <jammus@gmail.com>
2012-11-24 17:47:44 +0000

    Dot appears, disappears on touch, reappears.

--- src/com/jammus/pvt/GraphicsView.java
+++ src/com/jammus/pvt/GraphicsView.java
@@ -1,14 +1,19 @@
 package com.jammus.pvt;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.view.MotionEvent;
 import android.view.View;
 
 public class GraphicsView extends View {
 	private Paint circlePaint;
-	
 	private PerformTestActivity testActivity;
+	private long startTime;
+	private boolean isStimulusShown;
 	
 	public GraphicsView(Context context) {
 		super(context);
@@ -16,13 +21,51 @@ public class GraphicsView extends View {
 		testActivity = (PerformTestActivity) context;
 		
 		circlePaint = new Paint(getResources().getColor(R.color.stimulus));
+		
+		scheduleStimulus();
+	}
+	
+	private void showStimulus() {
+		isStimulusShown = true;
+		postInvalidate();
+	}
+	
+	private void scheduleStimulus() {
+		TimerTask task = new TimerTask() {
+			@Override
+			public void run() { 
+				startTime = scheduledExecutionTime();
+				showStimulus();
+			 }
+			
+		};
+		new Timer().schedule(task, 2000);
 	}
 	
 	@Override
 	public void onDraw(Canvas canvas) {
+		if (isStimulusShown) {
+			drawStimulus(canvas);
+		}
+	}
+	
+	@Override
+	public boolean onTouchEvent(MotionEvent e) {
+		if (! isStimulusShown) {
+			testActivity.registerError();
+		}
+		else {
+			isStimulusShown = false;
+			testActivity.registerScore();
+			scheduleStimulus();
+			postInvalidate();
+		}
+		return true;
+	}
+	
+	private void drawStimulus(Canvas canvas) {
 		int width = getWidth();
 		int height = getHeight();
-		
 		canvas.drawCircle(width / 2, height / 2, 100, circlePaint);
 	}
 }
--- src/com/jammus/pvt/PerformTestActivity.java
+++ src/com/jammus/pvt/PerformTestActivity.java
@@ -9,9 +9,20 @@ import android.os.Bundle;
 public class PerformTestActivity extends Activity {
 	public boolean showStimulus = false;
 	
+	private int errorCount = 0;
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(new GraphicsView(this));
+	}
+
+	public void registerError() {
+		errorCount++;
+	}
+
+	public void registerScore() {
+		// TODO Auto-generated method stub
+		
 	}
 }
