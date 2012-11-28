fee1f210f47c89d73529927303c0dfa2ea1cd078
jammus <jammus@gmail.com>
2012-11-28 21:37:13 +0000

    Move submission code into separate class.

--- src/com/jammus/pvt/activities/PerformTest.java
+++ src/com/jammus/pvt/activities/PerformTest.java
@@ -1,26 +1,9 @@
 package com.jammus.pvt.activities;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
-import java.util.ArrayList;
-import java.util.List;
-
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicNameValuePair;
-
 import com.jammus.pvt.R;
 import com.jammus.pvt.PvtResult;
 import com.jammus.pvt.data.PvtResultsDataStore;
+import com.jammus.pvt.data.PvtResultsSubmission;
 import com.jammus.pvt.data.sqlite.PvtResultsSQLiteDataStore;
 import com.jammus.pvt.views.Pvt;
 
@@ -83,63 +66,11 @@ public class PerformTest extends Activity {
 	}
 	
 	private class FetchReportTask extends AsyncTask<PvtResult, Void, String> {
-		private static final String POST_URL = "http://ec2-176-34-215-74.eu-west-1.compute.amazonaws.com/lalalalalala/";
-		private HttpResponse response;
 
 		@Override
 		protected String doInBackground(PvtResult... params) {
-			PvtResult result = params[0];
-			HttpPost httpPost = new HttpPost(POST_URL);
-			HttpClient httpClient = new DefaultHttpClient();
-			List<NameValuePair> postParams = new ArrayList<NameValuePair>(4);
-			postParams.add(new BasicNameValuePair("user_id", String.valueOf(userId)));
-			postParams.add(new BasicNameValuePair("date", String.valueOf(result.date())));
-			postParams.add(new BasicNameValuePair("errorCount", String.valueOf(result.errorCount())));
-			postParams.add(new BasicNameValuePair("averageRt", String.valueOf(result.averageRt())));
-			postParams.add(new BasicNameValuePair("responseTimes", responseTimesToString(result.responseTimes())));
-			try {
-				httpPost.setEntity(new UrlEncodedFormEntity(postParams));
-				response = httpClient.execute(httpPost);
-			} catch (UnsupportedEncodingException e) {
-				return e.getMessage();
-			} catch (ClientProtocolException e) {
-				return e.getMessage();
-			} catch (IOException e) {
-				return e.getMessage();
-			}
-			
-			HttpEntity httpEntity = response.getEntity();
-			if (httpEntity != null) {
-				InputStream inputStream;
-				try {
-					inputStream = httpEntity.getContent();
-				} catch (IllegalStateException e) {
-					return e.getMessage();
-				} catch (IOException e) {
-					return e.getMessage();
-				}
-				try {
-					BufferedReader buffer = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
-					StringBuilder sb = new StringBuilder();
-					String s;
-					while (true) {
-						s = buffer.readLine();
-						if (s == null || s.length() == 0) {
-							break;
-						}
-						sb.append(s);
-					}
-					buffer.close();
-					inputStream.close();
-					return sb.toString();
-				} catch (UnsupportedEncodingException e) {
-					return e.getMessage();
-				} catch (IOException e) {
-					return e.getMessage();
-				}
-			}
-			
-			return null;
+			PvtResultsSubmission submission = new PvtResultsSubmission();
+			return submission.submit(userId, params[0]);
 		}
 		
 		@Override
@@ -147,16 +78,6 @@ public class PerformTest extends Activity {
 			TextView reportView = (TextView) findViewById(R.id.report);
 			reportView.setText(result);
 		}
-		
-		private String responseTimesToString(float[] times) {
-			StringBuilder sb = new StringBuilder();
-			for (int index = 0; index < times.length; index++) {
-				if (index > 0) {
-					sb.append(",");
-				}
-				sb.append(String.valueOf(times[index]));
-			}
-			return sb.toString();
-		}
+	
 	}
 }
