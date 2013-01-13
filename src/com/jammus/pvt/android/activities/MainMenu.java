7af07f9e653fd147659bf1a0e4bc2be8eb54d32d
jammus <jammus@gmail.com>
2013-01-13 12:21:41 +0000

    Add login screen.

--- src/com/jammus/pvt/android/activities/MainMenu.java
+++ src/com/jammus/pvt/android/activities/MainMenu.java
@@ -24,9 +24,13 @@ public class MainMenu extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         user = fetchUser();
-        setContentView(R.layout.activity_start_screen);
-        TextView usernameView = (TextView) findViewById(R.id.username);
-        usernameView.setText("Signed in as: " + user.username());
+        if (user == null) {
+	        setContentView(R.layout.activity_login);
+        } else {
+	        setContentView(R.layout.activity_start_screen);
+	        TextView usernameView = (TextView) findViewById(R.id.username);
+	        usernameView.setText("Signed in as: " + user.email());
+        }
     }
 
     @Override
@@ -50,15 +54,17 @@ public class MainMenu extends Activity {
     private User fetchUser() {
         SharedPreferences settings = getSharedPreferences(USER_PREFS, 0);
         int userId = settings.getInt("user_id", -1);
-        String username = settings.getString("username", "");
-        if (userId <= 0) {
-        	userId = new Random().nextInt(3000);
+        String email = settings.getString("email", "");
+        String token = settings.getString("token", "");
+        if (userId <= 0 || token == "") {
+        	return null;
+        	/*userId = new Random().nextInt(3000);
         	username = "guest" + String.valueOf(userId);
         	Editor settingsEditor = settings.edit();
         	settingsEditor.putInt("user_id", userId);
         	settingsEditor.putString("username", username);
-        	settingsEditor.commit();
+        	settingsEditor.commit();*/
         }
-        return new User(userId, username);
+        return new User(userId, email, token);
     }
 }
--- src/com/jammus/pvt/core/User.java
+++ src/com/jammus/pvt/core/User.java
@@ -3,19 +3,25 @@ package com.jammus.pvt.core;
 public class User {
 
 	private final int id;
-	private final String username;
+	private final String email;
+	private final String token;
 	
-	public User(int id, String username) {
+	public User(int id, String email, String token) {
 		this.id = id;
-		this.username = username;
+		this.email = email;
+		this.token = token;
 	}
 	
 	public int id() {
 		return id;
 	}
 	
-	public String username() {
-		return username;
+	public String email() {
+		return email;
+	}
+	
+	public String token() {
+		return token;
 	}
 	
 }
