517e4340b2ae341d85de9f6796fdc80e32686e14
jammus <jammus@gmail.com>
2013-01-20 09:33:30 +0000

    Remove unused import

--- com.jammus.pvt/src/com/jammus/pvt/api/ApiClient.java
+++ com.jammus.pvt/src/com/jammus/pvt/api/ApiClient.java
@@ -1,7 +1,6 @@
 package com.jammus.pvt.api;
 
 import com.jammus.pvt.core.PvtResult;
-import com.jammus.pvt.core.User;
 
 public interface ApiClient {
 	String submitResult(String accessToken, PvtResult result) throws ApiTransportException;
